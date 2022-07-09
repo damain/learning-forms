327bde8e624638066af470547bb46997f1b77e75
Damain Joseph <damainjoseph@regiscorp.com>
2022-07-09 10:49:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Forms
+
+In this exercise you will learn about sending form data to a backend
+
+If you dont have node installed you can download it here [https://nodejs.org/en/](https://nodejs.org/en/)
+
+After installing node you can start up the server by typing (in the console) 
+```npm run start:server```
+
+This starts a server with the following endpoints
+|Endpoint| Purpose |
+|-----|-----|
+|/|default route returns some text when visited|              
+|/hello|expects to recieve a name and greets you|
+|/multiply|expects to receive a first and second value and multiplies them |
---
+++ server.js
@@ -0,0 +1,55 @@
+
+//Seting up the server
+const express = require('express')
+const app = express()
+const port = 3002
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+//Creating routes to respond when the url is called
+app.get('/', (req, res) => {
+    res.send('sup')
+})
+
+app.post('/hello', (req, res) => {
+    //print the body that was sent for troubleshooting 
+    console.dir(req.body)
+
+    //check for error
+    if (!req.body.name) {
+        res.send(`You didn't tell me your name, I got ${JSON.stringify(req.body)} instead`)
+        return
+    }
+
+    res.send(`hello ${req.body.name}`)
+})
+
+app.post('/multiply', (req, res) => {
+    //print the body that was sent for troubleshooting 
+    console.dir(req.body)
+
+    // get the first and second values from the body
+    const { first, second } = req.body
+
+    // set up message to hold string that will be returned to the user and default to displaying an error 
+    let message = `I got ${JSON.stringify(req.body)} . You didn't send:`
+    if (!first) {
+        message += ` [first] `
+    }
+    if (!second) {
+        message += ` [second] `
+    }
+    
+    // if we got both numbers then set message to: first * second = answer
+    if (first && second) {
+        message = ` ${first} x ${second} = ${Number(first) * Number(second)}`
+    }
+
+    res.send(message)
+})
+
+// make the server start listening for requests
+app.listen(port, () => {
+    console.log(`listening on port:${port} 🚀`)
+})
